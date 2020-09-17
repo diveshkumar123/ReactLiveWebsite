acd7d969d451b78ade79aa9d18d305aa505e583d
Divesh <diveshvats35@gmail.com>
2020-09-17 20:21:51 +0530

    Updates

---
+++ precache-manifest.f38248f9569d3b0c431e40886db5e433.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e83a46fcabeec961e6ef2ca7d5908546",
+    "url": "/appscriptTask/index.html"
+  },
+  {
+    "revision": "23b1b2b8274aac4844c0",
+    "url": "/appscriptTask/static/css/2.569a03b8.chunk.css"
+  },
+  {
+    "revision": "4a92e3030511d9d2f5df",
+    "url": "/appscriptTask/static/css/main.8a2cca6a.chunk.css"
+  },
+  {
+    "revision": "23b1b2b8274aac4844c0",
+    "url": "/appscriptTask/static/js/2.ff28c3ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/appscriptTask/static/js/2.ff28c3ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a92e3030511d9d2f5df",
+    "url": "/appscriptTask/static/js/main.34e75469.chunk.js"
+  },
+  {
+    "revision": "623cd092502b20e73708",
+    "url": "/appscriptTask/static/js/runtime-main.a5578798.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/appscriptTask/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/appscriptTask/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/appscriptTask/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/appscriptTask/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/appscriptTask/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
